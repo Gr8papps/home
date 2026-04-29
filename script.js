ed865a9d6b4e2d0b78459286c2e24e63cd819922
gr8pfart <ethan_whits484@ahschools.us>
2026-04-29 09:37:56 -0500

    Update URLs to use gn-math assets and HTML

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const zonesurls = [
     "https://cdn.jsdelivr.net/gh/gn-math/assets/zones.json"
 ];
 let zonesURL = zonesurls[Math.floor(Math.random() * zonesurls.length)];
-const coverURL = "https://cdn.jsdelivr.net/gh/Gr8papps/gr8papps.github.io-home-cover/";
-const htmlURL = "https://cdn.jsdelivr.net/gh/Gr8papps/gr8papps.github.io-home-games/";
+const coverURL = "https://cdn.jsdelivr.net/gh/gn-math/covers@main";
+const htmlURL = "https://cdn.jsdelivr.net/gh/gn-math/html@main";
 let zones = [];
 let popularityData = {};
 const featuredContainer = document.getElementById('featuredZones');
@@ -29,7 +29,7 @@ async function listZones() {
             shajson = await sharesponse.json();
             sha = shajson[0]['sha'];
             if (sha) {
-                zonesURL = `https://cdn.jsdelivr.net/gh/Gr8papps/home/@${sha}/zones.json`;
+                zonesURL = `https://cdn.jsdelivr.net/gh/gn-math/assets@${sha}/zones.json`;
             }
           } catch (error) {
             try {
@@ -37,7 +37,7 @@ async function listZones() {
                 if (secondarysharesponse && secondarysharesponse.status === 200) {
                     sha = (await secondarysharesponse.text()).trim();
                     if (sha) {
-                        zonesURL = `https://cdn.jsdelivr.net/gh/Gr8papps/home/@${sha}/zones.json`;
+                        zonesURL = `https://cdn.jsdelivr.net/gh/gn-math/assets@${sha}/zones.json`;
                     }
                 }
             } catch(error) {}
