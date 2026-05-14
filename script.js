1c397dd56088a504e872f5d5c6d43e595a3a0694
gr8pfart <ethan_whits484@ahschools.us>
2026-05-14 06:33:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const searchBar = document.getElementById('searchBar');
 // https://www.jsdelivr.com/tools/purge
 const zonesurls = [
     "https://raw.githack.com/Gr8papps/home/main/zone.json"
-    
+    ];
+let zonesURL = zonesurls[Math.floor(Math.random() * zonesurls.length)];
 const coverURL = "https://cdn.jsdelivr.net/gh/gr8pfart/assets@main/covers/";
 const htmlURL = "https://cdn.jsdelivr.net/gh/gr8pfart/assets@main/games/";
 let zones = [];
